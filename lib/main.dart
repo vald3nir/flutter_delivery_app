5d396673f048050c407ce5be34a7933c6b45a298
Valdenir Severino <vald3nir@gmail.com>
2023-02-01 14:35:56 -0300

    use dot env

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'app/core/config/env/env.dart';
 import 'app/dw9_delivery_app.dart';
 
-void main() {
+Future<void> main() async {
+  await Env.instance.load();
   runApp(const Dw9DeliveryApp());
 }
